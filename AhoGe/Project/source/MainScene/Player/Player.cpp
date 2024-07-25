c53fd7ffba872f6d11e27cc79a2aec21832dc492
emiya_ubw <a6m3.zero.f@gmail.com>
2024-07-25 22:00:36 +0900

    gg

--- AhoGe/Project/source/MainScene/Player/Player.cpp
+++
@@ -1,8 +0,0 @@
-#include	"Player.hpp"
-#include	"../../sub.hpp"
-
-const FPS_n2::Sceneclass::PlayerManager* SingletonBase<FPS_n2::Sceneclass::PlayerManager>::m_Singleton = nullptr;
-namespace FPS_n2 {
-	namespace Sceneclass {
-	};
-};
