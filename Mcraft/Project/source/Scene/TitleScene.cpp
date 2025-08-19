e8e47388e497a6935acf0e3e11f7999029a6ea88
emiya_ubw <a6m3.zero.f@gmail.com>
2025-08-19 22:36:29 +0900

    awda

--- Mcraft/Project/source/Scene/TitleScene.cpp
+++ Mcraft/Project/source/Scene/TitleScene.cpp
@@ -651,7 +651,7 @@ namespace FPS_n2 {
 				// 
 				DrawCtrls->SetString(WindowSystem::DrawLayer::Normal, FontSystem::FontType::MS_Gothic, (18),
 					FontSystem::FontXCenter::RIGHT, FontSystem::FontYCenter::TOP,
-					64 + 369, 64 + 207, White, Black, "Ver 1.0.0");
+					64 + 369, 64 + 207, White, Black, "Ver 1.0.3");
 				// 
 				ButtonParts->Draw();
 				//
